68ce5d6d136bff111b1a77749a29143b369784ba
João Brocca <162700492+JoaoPedroB08@users.noreply.github.com>
2025-09-16 09:14:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const btnSubir = document.getElementById("btn-subir");
 const btnDescer = document.getElementById("btn-descer");
-const navbar = document.getElementById("navbar");
 const rodape = document.getElementById("rodape");
 
 // Ao clicar em subir
 btnSubir.addEventListener("click", () => {
-    navbar.scrollIntoView({ behavior: "smooth" });
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 });
 
 // Ao clicar em descer
 btnDescer.addEventListener("click", () => {
     rodape.scrollIntoView({ behavior: "smooth" });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerBtn = document.getElementById('hamburger-btn');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburgerBtn.addEventListener('click', function() {
+        // Alterna a classe 'active' no container dos links
+        navLinks.classList.toggle('active');
+    });
 });
